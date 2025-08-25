b59f3e6c0eaf1e18e12c7068a0f765d251802701
rakesh6390 <rkchauhan155223@gmail.com>
2025-08-25 10:59:14 +0530

    admin authentication

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: 'https://urvann-lemon.vercel.app', // Replace with your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }))
